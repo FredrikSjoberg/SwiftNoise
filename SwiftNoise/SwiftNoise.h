ef6a13fb7979a2552d4a8db8422200ad5e6d2609
FredrikSjoberg <fredriksjoberg@Fredriks-iMac.lan>
2015-07-16 21:15:24 +0200

    Initial Commit

---
+++ SwiftNoise/SwiftNoise.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNoise.h
+//  SwiftNoise
+//
+//  Created by Fredrik Sjöberg on 16/07/15.
+//  Copyright (c) 2015 Fredrik Sjoberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNoise.
+FOUNDATION_EXPORT double SwiftNoiseVersionNumber;
+
+//! Project version string for SwiftNoise.
+FOUNDATION_EXPORT const unsigned char SwiftNoiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNoise/PublicHeader.h>
+
+
